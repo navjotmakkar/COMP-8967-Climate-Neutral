b020bc713fae777779ab43324bef50fd707bf499
Aakash Lakra <aaklakra@gmail.com>
2023-10-26 22:37:06 -0400

    Added file for csv upload

---
+++ js/fileupload.js
@@ -0,0 +1,36 @@
+document.getElementById("convertButton").addEventListener("click", convertToJSON);
+
+function convertToJSON() {
+    const fileInput = document.getElementById("csvFile");
+    const file = fileInput.files[0];
+
+    if (!file) {
+        console.log("Please select a CSV file.");
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const contents = e.target.result;
+        const lines = contents.split("\n");
+
+        const data = [];
+        const headers = lines[0].split(",");
+
+        for (let i = 1; i < lines.length; i++) {
+            const currentLine = lines[i].split(",");
+            if (currentLine.length === headers.length) {
+                const entry = {};
+                for (let j = 0; j < headers.length; j++) {
+                    entry[headers[j]] = currentLine[j];
+                }
+                data.push(entry);
+            }
+        }
+
+        const jsonData = JSON.stringify(data, null, 2);
+        console.log(jsonData);
+    };
+
+    reader.readAsText(file);
+}
